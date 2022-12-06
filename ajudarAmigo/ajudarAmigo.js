deb3905d263a1f295a3c45cac4453ac20c024905
Michi Naka <gmassamichi@gmail.com>
2022-12-06 18:49:46 -0300

    save

---
+++ ajudarAmigo/ajudarAmigo.js
@@ -0,0 +1,70 @@
+$(document).ready(function(){
+    $("#action1").click(function(){
+        document.getElementById("a1").innerHTML = "<p> &nbsp;&nbsp;&nbsp;</p>"
+        $("#a1").hide().slideDown(1000);
+        $(".header3-2").css("margin-top","4cm")
+    });
+});
+$(document).ready(function(){
+    $("#action2").click(function(){
+        document.getElementById("a2").innerHTML = "<p> &nbsp;&nbsp;&nbsp;</p>"
+        $("#a2").hide().slideDown(1000);
+        $(".header3-2").css("margin-top","4cm")
+    });
+});
+$(document).ready(function(){
+    $("#action3").click(function(){
+        document.getElementById("a3").innerHTML = "<p>&nbsp;&nbsp;&nbsp;</p>"
+        $("#a3").hide().slideDown(1000);
+        $(".header3-2").css("margin-top","4cm")
+    });
+});
+$(document).ready(function(){
+    $("#action4").click(function(){
+        document.getElementById("a4").innerHTML = "<p> &nbsp;&nbsp;&nbsp;</p>"
+        $("#a4").hide().slideDown(1000);
+        $(".header3-3").css("margin-top","4cm")
+    });
+});
+$(document).ready(function(){
+    $("#action5").click(function(){
+        document.getElementById("a5").innerHTML = "<p> &nbsp;&nbsp;&nbsp;</p>"
+        $("#a5").hide().slideDown(1000);
+        $(".header3-3").css("margin-top","4cm")
+    });
+});
+$(document).ready(function(){
+    $("#action6").click(function(){
+        document.getElementById("a6").innerHTML = "<p> &nbsp;&nbsp;&nbsp;</p>"
+        $("#a6").hide().slideDown(1000);
+        $(".header3-3").css("margin-top","4cm")
+    });
+});
+$(document).ready(function(){
+    $("#action7").click(function(){
+        document.getElementById("a7").innerHTML = "<p> &nbsp;&nbsp;&nbsp;</p>"
+        $("#a7").hide().slideDown(1000);
+        $(".header5").css("margin-top","4cm")
+    });
+});
+$(document).ready(function(){
+    $("#action8").click(function(){
+        document.getElementById("a8").innerHTML = "<p>&nbsp;&nbsp;&nbsp;</p>"
+        $("#a8").hide().slideDown(1000);
+        $(".header5").css("margin-top","4cm")
+    });
+});
+$(document).ready(function(){
+    $("#action9").click(function(){
+        document.getElementById("a9").innerHTML = "<p> &nbsp;&nbsp;&nbsp;</p>"
+        $("#a9").hide().slideDown(1000);
+        $(".header5").css("margin-top","4cm")
+    });
+});
+
+$(document).ready(function(){
+    $(".image1").hide().fadeIn(3000);
+});
+$(document).ready(function(){
+    $(".image2").hide().fadeIn(3000);
+});
