e0553b968be1d3c9b98b9a6094fa2425dfc99701
Michi Naka <gmassamichi@gmail.com>
2022-12-06 17:26:48 -0300

    save

---
+++ mitos/mitos.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+    $("#action1").mouseenter(function(){
+        document.getElementById("a1").innerHTML = "<p> &nbsp;&nbsp;&nbsp;O transtorno de ansiedade generalizada é uma das doenças mentais mais comuns. O impacto que tem na vida da pessoa é imenso, caótico e desgastante. Assim, e caso alguém do entorno desse paciente comente que esta realidade que sofre é sua responsabilidade por “fazer as coisas errado”, aumentará ainda mais o abatimento e reduzirá a vontade de encontrar soluções.</p>"
+    });
+});
+$(document).ready(function(){
+    $("#action2").mouseenter(function(){
+        document.getElementById("a2").innerHTML = "<p> &nbsp;&nbsp;&nbsp;Este é, sem dúvidas, outro dos mitos sobre a ansiedade que são mais recorrentes. Há quem pense que a ansiedade faz parte de sua própria personalidade e, portanto, não há nada a fazer, não haverá terapia e nem tratamento que possa remediá-la. Pensa que é assim e ponto. Identifica a ansiedade como parte de seu ser, como uma emoção inerente a sua personalidade. <br> &nbsp;&nbsp;&nbsp;Mudemos o enfoque e assumamos uma visão mais realista, lógica e otimista da ansiedade e de qualquer outro tipo de transtorno. Todos podemos integrar novos estilos de pensamento, gerenciar melhor nossas emoções, mudar condutas, hábitos e inclusive reprogramar nosso cérebro para dar-lhe calma, para melhorar seu foco…</p>"
+    });
+});
+$(document).ready(function(){
+    $("#action3").mouseenter(function(){
+        document.getElementById("a3").innerHTML = "<p>&nbsp;&nbsp;&nbsp;Evitar a ansiedade tende a reforçá-la. De acordo com a Anxiety and Depression Association of America (ADAA), suprimir seus pensamentos torna-os mais fortes e frequentes. Esquivar-se do sentimento não é uma boa saída, pois passa a impressão de que nada está acontecendo — e quanto mais se evita o problema, pior ele fica. Inclusive, em fobias, as técnicas costumam ser de enfrentamento e não de evitação – passo a passo o paciente é aproximado do motivo da fobia. </p>"
+        $(".header3-2").css("margin-top","0")
+    });
+});
+$(document).ready(function(){
+    $("#action4").mouseenter(function(){
+        document.getElementById("a4").innerHTML = "<p> &nbsp;&nbsp;&nbsp;Sofrer de depressão não significa que você seja uma pessoa fraca. Quando alguém sofre com essa doença, as pessoas a veem como alguém incapaz de enfrentar as dificuldades, como alguém que não é suficientemente forte para encarar a vida. A depressão é um problema psicológico que vira a vida de cabeça para baixo, afetando-nos psicologicamente, biologicamente e socialmente.&nbsp;&nbsp;&nbsp;Sofrer de depressão não é ser fraco. Você não escolheu ter depressão! Não tem uma mente fraca!  Esse mito faz com que as pessoas se isolem e sofram sua doença em completo silêncio, sofrendo ainda mais, se é que é possível.</p>"
+    });
+});
+$(document).ready(function(){
+    $("#action5").mouseenter(function(){
+        document.getElementById("a5").innerHTML = "<p> &nbsp;&nbsp;&nbsp;A depressão, longe do que podemos crer, não afeta apenas a mente, mas também o corpo de maneira geral. A insônia, a fadiga, as dores musculares crônicas, as dores de cabeça, são alguns dos sintomas que podem aparecer. Se você acredita que é algo que existe apenas em nossa cabeça, quase como uma invenção, saiba que este é um erro grave. A depressão não é apenas mental, vai muito além.&nbsp;&nbsp;&nbsp;As pessoas que nunca tiveram contato com alguém que já sofreu de depressão podem pensar dessa maneira um pouco superficial, mas ninguém quer inventar e desejar ter uma doença tão grave como é a depressão.</p>"
+    });
+});
+$(document).ready(function(){
+    $("#action6").mouseenter(function(){
+        document.getElementById("a6").innerHTML = "<p> &nbsp;&nbsp;&nbsp;O primeiro destes mitos sobre a depressão diz algo que todos conhecemos. Quando nos dizem que uma pessoa esta sofrendo de depressão, imediatamente pensamos que ela está muito triste. Nada mais longe da realidade.&nbsp;&nbsp;&nbsp;Todo sentimos tristeza em certos momentos da vida, mas há um abismo entre a tristeza e a depressão. A tristeza é passageira e temporária. Cedo ou tarde ela se vai.&nbsp;&nbsp;&nbsp;A depressão é o contrário. Não é algo passageiro, é algo que fica junto com muitas outras coisas: apatia, sentimento de vazio, dor… A depressão não vem sozinha, mas sim acompanhada de muitos outros sentimentos negativos que fazem com que toda a sua vida seja cinza.</p>"
+        $(".header3-3").css("margin-top","0")
+    });
+});
+$(document).ready(function(){
+    $("#action7").mouseenter(function(){
+        document.getElementById("a7").innerHTML = "<p> &nbsp;&nbsp;&nbsp;Quando usamos as frases acima, estamos nos referindo às emoções, pois acreditamos que esse transtorno afete apenas isso, mas isso é um completo mito. A pessoa que tem bipolaridade também sofre de alterações e deficiências no julgamento racional, dificuldade de concentração e memória. Além disso, ela também pode ter problemas com seu apetite sexual e alterações em seus níveis de energia. Essas características podem levar à depressão ou baixa autoestima.</p>"
+    });
+});
+$(document).ready(function(){
+    $("#action8").mouseenter(function(){
+        document.getElementById("a8").innerHTML = "<p>&nbsp;&nbsp;&nbsp;Muitas pessoas pensam que o transtorno bipolar pode ser comparado às mudanças de humor de alguém. Por exemplo, quando um ente querido morre, perde um exame ou em alguma outra situação triste do dia a dia. No entanto, é completamente diferente. Mudanças de humor em pessoas com alteração bipolar afetam seus dias e suas habilidades.</p>"
+    });
+});
+$(document).ready(function(){
+    $("#action9").mouseenter(function(){
+        document.getElementById("a9").innerHTML = "<p> &nbsp;&nbsp;&nbsp;Muitas pessoas com transtorno bipolar podem não procurar ajuda porque se sentem frágeis se não forem capazes de controlar suas emoções. No entanto, sofrer de depressão, ansiedade ou qualquer outro tipo de doença não o torna mais frágil do que os outros. O transtorno bipolar é simplesmente uma condição que pode ser tratada. Assim, a pessoa que a sofre consegue atingir o equilíbrio de que necessita.&nbsp;&nbsp;&nbsp;Precisamos limpar esses mitos sobre o transtorno bipolar de nossas mentes, porque eles nos impedem de aprofundar nosso conhecimento e descobrir o que ele realmente é. Acreditar que quem sofre é louco ou pode enlouquecer os outros não é uma atitude madura para um problema que esgota e desgasta quem sofre</p>"
+        $("h5").css("margin-top","0")
+    });
+});
